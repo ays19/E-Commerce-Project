fd00d06514e706cb2a5bcdc0cfd795a210c56549
Ahsan Yasir Sharar <sahsanyasir@gmail.com>
2025-12-30 12:24:04 +0600

    create product details & connect the path

--- config/product/urls.py
+++ config/product/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 
 from .views import (
-Home
+Home,
+ProductDetails,
 )
 
 urlpatterns= [
 path('', Home.as_view(), name='home'),
+path('product-details/', ProductDetails.as_view(), name='product-details'),
 ]
