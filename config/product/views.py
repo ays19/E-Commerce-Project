b4a52690a202268719fd892f9220228eec04bded
Ahsan Yasir Sharar <sahsanyasir@gmail.com>
2025-12-30 12:15:38 +0600

    create a new class for products

--- config/product/views.py
+++ config/product/views.py
@@ -4,3 +4,7 @@ from django.views import generic
 
 class Home(generic.TemplateView):
 	template_name= 'home.html'
+
+class ProductDetails(generic.TemplateView):
+	template_name= 'product/product-details.html'
+
