bb945dd45c337e0cbbabd8b3672568e002dbb92e
ays19 <sahsanyasir@gmail.com>
2026-01-02 17:05:20 +0600

    code in create data

---
+++ config/product/management/command/create_data.py
@@ -0,0 +1,25 @@
+from typing import Any
+from django.core.management.base import BaseCommand
+from config.product.models import Category, Product
+import requests
+from django.utils.text import slugify
+
+class Command(BaseCommand):
+    def handle(self, *args: **options):
+    print("Adding producrt")
+    response = requests.get('https://fakestoreapi.com/products').json()
+
+    for product in response:
+        category, _ = Category.objects.get_or_create(
+            title=Product['category'],
+            slug=slugify(Product['category']),
+            featured=True
+        )
+        Product.objects.get_or_create(
+            title=Product['title'],
+            slug=slugify(Product['title']),
+            description=Product['description'],
+            price=Product['price'],
+            category=category,
+            image=Product['image']
+        )
