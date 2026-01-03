b36f3c35e186c4670788f433f898fd632b695cd7
ays19 <sahsanyasir@gmail.com>
2026-01-03 21:41:24 +0600

    edited CartItem.__str__ and others

--- config/product/models.py
+++ config/product/models.py
@@ -1,33 +1,44 @@
 from django.db import models
 from django.conf import settings
 
+
 class Category(models.Model):
-    title= models.CharField(max_length=150, unique=True)
-    slug= models.SlugField(unique=True)
-    featured= models.BooleanField(default=False)
-    created_date= models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(unique=True)
+    featured = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering= ['title']
+        ordering = ["title"]
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.title
-    
+
+
 class Product(models.Model):
-    category= models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    title= models.CharField(max_length=150, unique=True)
-    slug= models.SlugField(unique=True)
-    featured= models.BooleanField(default=False)
+    category = models.ForeignKey(
+        Category,
+        related_name="products",
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(unique=True)
+    featured = models.BooleanField(default=False)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     count = models.PositiveIntegerField(default=0)
-    description= models.TextField(null=True, blank=True, default='N/A')
-    created_date= models.DateTimeField(auto_now_add=True)
-    updated_date= models.DateTimeField(auto_now=True)
-    thumbnail= models.URLField()
+    description = models.TextField(null=True, blank=True, default="N/A")
+    thumbnail = models.URLField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
 
 class Cart(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
+        related_name="carts",
         on_delete=models.CASCADE,
         null=True,
         blank=True
@@ -48,20 +59,21 @@ class CartItem(models.Model):
         Product,
         on_delete=models.CASCADE
     )
-    quantity = models.PositiveIntegerField(default=1)    
+    quantity = models.PositiveIntegerField(default=1)
 
     class Meta:
-        ordering= ['-id']
+        ordering = ["-id"]
+        unique_together = ("cart", "product")
 
-    def __str__(self) -> str:
-        return self.title
-    
+    def __str__(self):
+        return self.product.title
+
+
 class Slider(models.Model):
-    title= models.CharField(max_length=150, unique=True)
-    banner= models.ImageField(upload_to='banners')
-    show= models.BooleanField(default=True)
-    created_date= models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=150, unique=True)
+    banner = models.ImageField(upload_to="banners/")
+    show = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.title
-
