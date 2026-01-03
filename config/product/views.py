ddd16d93f13d3c8f8f3c081596d62ecc9148d1ef
Ahsan Yasir Sharar <sahsanyasir@gmail.com>
2026-01-03 14:39:07 +0600

    more powerful views.py

--- config/product/views.py
+++ config/product/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Product, Slider, Category
 
-from django.views import generic
 
-class Home(generic.TemplateView):
-	template_name= 'home.html'
+class Home(ListView):
+    model = Product
+    template_name = "home.html"
+    context_object_name = "products"
 
-class ProductDetails(generic.TemplateView):
-	template_name= 'product/product-details.html'
+    def get_queryset(self):
+        return Product.objects.filter(featured=True, count__gt=0)
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["sliders"] = Slider.objects.filter(show=True)
+        context["categories"] = Category.objects.all()
+        return context
+
+
+class ProductDetails(DetailView):
+    model = Product
+    template_name = "product/product-details.html"
+    context_object_name = "product"
+    slug_field = "slug"
+    slug_url_kwarg = "slug"
