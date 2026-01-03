5717609c602bd20393e5d482bc679419643edccd
ays19 <sahsanyasir@gmail.com>
2026-01-03 08:57:53 +0600

    STATIC_ROOT = BASE_DIR / 'staticfiles' ADD THIS

--- config/config/settings.py
+++ config/config/settings.py
@@ -125,6 +125,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
 STATICFILES_DIRS= [BASE_DIR / 'static',]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL= 'user_account.user'
