8fcf329045375dfc528c33f613d26fdd750ab160
Ahsan Yasir Sharar <sahsanyasir@gmail.com>
2026-01-03 12:51:41 +0600

    update admin ,sqllite

--- config/product/admin.py
+++ config/product/admin.py
@@ -1,19 +1,22 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Slider
 
 # Register your models here.
-from .models import (
-    Category,
-    Product,
-    Slider
-)
-
-class ProductAdmin(admin.ModelAdmin):
-    prepopulated_fields= {"slug": ('title',)}
-
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields= {"slug": ('title',)}
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ("title", "featured", "created_date")
+    search_fields = ("title",)
 
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Product, ProductAdmin)
-admin.site.register(Slider)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ("title", "price", "count", "featured")
+    list_filter = ("featured", "category")
+    search_fields = ("title",)
+
+@admin.register(Slider)
+class SliderAdmin(admin.ModelAdmin):
+    list_display = ("title", "show", "created_date")
+    list_filter = ("show",)
+    search_fields = ("title",)
