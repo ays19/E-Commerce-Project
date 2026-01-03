4cf4fcb8e7aa0d719d80a3c184ebde7c5e286576
Ahsan Yasir Sharar <sahsanyasir@gmail.com>
2026-01-03 11:29:13 +0600

    updating create data

--- config/product/management/commands/create_data.py
+++ config/product/management/commands/create_data.py
@@ -1,15 +1,20 @@
-from typing import Any
-from django.core.management import BaseCommand
+from django.core.management.base import BaseCommand
 from product.models import Category, Product
-import requests
 from django.utils.text import slugify
+import requests
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        print("Adding producrt")
-        response = requests.get('https://fakestoreapi.com/products').json()
+        print("Adding producrt...")
+        url = "https://fakestoreapi.com/products"
+        response = requests.get(url, timeout=10)
 
-        for product in response:
+        if response.status_code != 200:
+            self.stderr.write("Failed to fetch data from API")
+            return
+        products = response.json()
+        
+        for product in products:
             category, _ = Category.objects.get_or_create(
                 title=product['category'],
                 slug=slugify(product['category']),
