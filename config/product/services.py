f62742f87c8484901676a8a4badf234469f614f8
ays19 <sahsanyasir@gmail.com>
2026-01-05 06:54:45 +0600

    function for cart

--- config/product/services.py
+++ config/product/services.py
@@ -1,5 +1,5 @@
 from product.models import Product
-
+from product.models import Cart, CartItem
 
 def reduce_stock(product: Product, quantity: int) -> None:
     if quantity <= 0:
@@ -10,3 +10,10 @@ def reduce_stock(product: Product, quantity: int) -> None:
 
     product.count -= quantity
     product.save(update_fields=["count"])
+
+def get_or_create_cart(user):
+    """
+    Returns existing cart or creates a new one for the user
+    """
+    cart, created = Cart.objects.get_or_create(user=user)
+    return cart
