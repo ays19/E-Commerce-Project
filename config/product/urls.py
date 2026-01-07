bc17b0402d7704eeb2e0a122d09c21f0cfa1eadc
Ahsan Yasir Sharar <sahsanyasir@gmail.com>
2026-01-07 16:07:49 +0600

    cart: configure cart-related URLs

--- config/product/urls.py
+++ config/product/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import Home, ProductDetails
+from .views import Home, ProductDetails, CartView, add_to_cart, remove_from_cart
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('product/<slug:slug>/', ProductDetails.as_view(), name='product-details'),
+    path('cart/', CartView.as_view(), name='cart'),
+    path('add-to-cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
+    path('remove-from-cart/<int:item_id>/', remove_from_cart, name='cart_remove'),
 ]
