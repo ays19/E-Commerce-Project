76450ba41aac2a56e2d75d520b7e323b18adcbdd
ays19 <sahsanyasir@gmail.com>
2026-01-07 21:43:17 +0600

    OrderItem models

--- config/product/models.py
+++ config/product/models.py
@@ -79,6 +79,22 @@ class Order(models.Model):
     def __str__(self):
         return f"Order #{self.id}"
 
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        related_name='items',
+        on_delete=models.CASCADE
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE
+    )
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.product.title
+
 
 class Slider(models.Model):
     title = models.CharField(max_length=150, unique=True)
