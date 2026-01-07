8e09333b1b8c960fcbc4c7b35644b056a0e76597
ays19 <sahsanyasir@gmail.com>
2026-01-07 21:39:41 +0600

    order: add Order

--- config/product/models.py
+++ config/product/models.py
@@ -67,6 +67,17 @@ class CartItem(models.Model):
 
     def __str__(self):
         return self.product.title
+    
+class Order(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Order #{self.id}"
 
 
 class Slider(models.Model):
